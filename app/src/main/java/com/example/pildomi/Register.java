1a2b884caf22b3552c49f15b61d8df073374c737
c0der97 <bare-o-emilio15@hotmail.com>
2020-07-03 22:36:42 -0500

    paso de información entre actividades

--- app/src/main/java/com/example/pildomi/Register.java
+++ app/src/main/java/com/example/pildomi/Register.java
@@ -2,13 +2,41 @@ package com.example.pildomi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
+
+    private EditText name;
+    private EditText lastname;
+    private EditText Age;
+    private EditText Password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Intent intent = new Intent(this, Start.class);
+        saveData(intent);
+    }
+
+    protected void saveData(final Intent intent){
+        final Button _register = (Button) findViewById(R.id._register);
+         name = (EditText) findViewById(R.id._name);
+        lastname = (EditText) findViewById(R.id._name2);
+        Age = (EditText) findViewById(R.id.age);
+        Password = (EditText) findViewById(R.id._password);
+        _register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent.putExtra("name", name.getText().toString());
+                intent.putExtra("lastname", lastname.getText().toString());
+                intent.putExtra("age", Age.getText().toString());
+                intent.putExtra("password", Password.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pildomi/Start.java
+++ app/src/main/java/com/example/pildomi/Start.java
@@ -3,12 +3,28 @@ package com.example.pildomi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Start extends AppCompatActivity {
+
+    private TextView _name;
+    private TextView age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        StartLoad();
+    }
+
+    protected  void StartLoad(){
+        String name = getIntent().getStringExtra("name");
+        String lastname = getIntent().getStringExtra("lastname");
+        String age = getIntent().getStringExtra("age");
+        this._name = (TextView)findViewById(R.id._name_);
+        this.age = (TextView)findViewById(R.id._age_);
+        this._name.setText("Welcome "+name+" "+lastname);
+        this.age.setText("Age  "+age);
     }
 }
